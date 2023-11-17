466912db58b20137f938da766bdf004b5230ba44
alex02r <alex.giochi02@gmail.com>
2023-11-17 14:01:53 +0100

    [UPGRADE]Add function preferite color

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@
 let name = prompt('Inserisci il tuo nome utente');
 //Ask Last name at user
 let lastName = prompt('Inserisci il tuo cognome');
+//Ask what is it preferite color of user
+let color = prompt('Inserisci il tuo colore preferito');
