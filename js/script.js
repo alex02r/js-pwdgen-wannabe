04d15b762efa7b84de3cba7228e0f1aaf360b332
alex02r <alex.giochi02@gmail.com>
2023-11-17 14:06:42 +0100

    [UPGRADE]Add function for generate password

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ let name = prompt('Inserisci il tuo nome utente');
 let lastName = prompt('Inserisci il tuo cognome');
 //Ask what is it preferite color of user
 let color = prompt('Inserisci il tuo colore preferito');
+
+//Write password generate on html file
+//generate messagge for html
+let password = `${name}${lastName}${color}23`;
+
+document.getElementById('psw').innerHTML = password;
